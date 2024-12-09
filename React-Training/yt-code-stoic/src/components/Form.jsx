72b16d312b5c7d1e1a44c9d4d60cf6f881c99463
nayem73 <nayem73@gmail.com>
2024-12-09 18:28:59 +0600

    This is how the video showed to create a temp function in order to pass the event to handleChange function

--- React-Training/yt-code-stoic/src/components/Form.jsx
+++ React-Training/yt-code-stoic/src/components/Form.jsx
@@ -4,13 +4,20 @@ export default function () {
   const [name, setName] = useState("");
   function handleChange(event) {
     console.log(event.target.value);
+    console.log(event);
     setName(event.target.value);
   }
 
   return (
     <div>
       <form>
-        <input onChange={handleChange} type="text" value={name} />
+        <input
+          onChange={function temp(event) {
+            return handleChange(event);
+          }}
+          type="text"
+          value={name}
+        />
       </form>
     </div>
   );
