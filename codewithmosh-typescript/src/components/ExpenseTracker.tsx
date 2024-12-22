f3936bedcdf8230a8c57efcaef2befc6a7d7bf8f
nayem73 <nayem73@gmail.com>
2024-12-22 07:22:24 +0600

    React-hook-form : form building completed

--- codewithmosh-typescript/src/components/ExpenseTracker.tsx
+++ codewithmosh-typescript/src/components/ExpenseTracker.tsx
@@ -1,23 +1,30 @@
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 
 function ExpenseTracker() {
   const { register, handleSubmit } = useForm();
-  console.log(register('description'));
+
+  const onSubmit = (data: FieldValues) => console.log(data)
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
-      <label>
-        Description:
-        <input type="text" {...register('description')} />
-      </label>
-      <label>
-        Amount:
-        <input type="number" {...register('amount')} />
-      </label>
-      <label>
-        Category:
-        <input type="text" {...register('category')} />
-      </label>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label>
+          Description:
+          <input type="text" {...register('description')} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Amount:
+          <input type="number" {...register('amount')} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Category:
+          <input type="text" {...register('category')} />
+        </label>
+      </div>
       <button type="submit">Add</button>
     </form>
   )
